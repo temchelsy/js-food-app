9939f5d4654faa5618d162085018982a32e2673e
tem chelsy <temchelsy@gmail.com>
2024-04-19 16:40:08 +0200

    modified content

--- assert/app.js
+++ assert/app.js
@@ -1,28 +1,27 @@
-
-const mealList = document.getElementById('mealList');
-const searchInput = document.getElementById('searchInput');
-let allMeals = [];
-
+const mealList = document.getElementById("mealList")
+const searchInput = document.getElementById("searchInput")
+let allMeals = []
 
 async function getMeals() {
-    try {
-        const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-        const data = await response.json();
-        allMeals = data.meals; 
-        displayMeals(allMeals); 
-    } catch (error) {
-        console.error('Error fetching meals:', error);
-    }
+  try {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/search.php?s="
+    )
+    const data = await response.json()
+    allMeals = data.meals
+    displayMeals(allMeals)
+  } catch (error) {
+    console.error("Error fetching meals:", error)
+  }
 }
 
-
 function displayMeals(meals) {
-    mealList.innerHTML = '';
+  mealList.innerHTML = ""
 
-    meals.forEach(meal => {
-        const mealItem = document.createElement('div');
-        mealItem.classList.add('meal-item');
-        mealItem.innerHTML = `
+  meals.forEach((meal) => {
+    const mealItem = document.createElement("div")
+    mealItem.classList.add("meal-item")
+    mealItem.innerHTML = `
             <div class="meal-image">
                 <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
             </div>
@@ -32,18 +31,17 @@ function displayMeals(meals) {
                 <p>Area: ${meal.strArea}</p>
                 <p>Instructions: ${meal.strInstructions}</p>
             </div>
-        `;
-        mealList.appendChild(mealItem);
-    });
+        `
+    mealList.appendChild(mealItem);
+  })
 }
 
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value.trim().toLowerCase()
+  const filteredMeals = allMeals.filter((meal) =>
+    meal.strMeal.toLowerCase().includes(searchTerm)
+  )
+  displayMeals(filteredMeals)
+})
 
-searchInput.addEventListener('input', () => {
-    const searchTerm = searchInput.value.trim().toLowerCase();
-    const filteredMeals = allMeals.filter(meal =>
-        meal.strMeal.toLowerCase().includes(searchTerm)
-    );
-    displayMeals(filteredMeals);
-});
-
-getMeals(); 
+getMeals()
