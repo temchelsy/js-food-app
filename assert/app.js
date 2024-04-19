7968aa123b8226d045cf7d23787bf11de241ce03
tem chelsy <temchelsy@gmail.com>
2024-04-19 17:01:47 +0200

    modified content

--- assert/app.js
+++ assert/app.js
@@ -1,26 +1,26 @@
-const mealList = document.getElementById("mealList")
-const searchInput = document.getElementById("searchInput")
+const mealList = document.getElementById('mealList')
+const searchInput = document.getElementById('search-input')
 let allMeals = []
 
-async function getMeals() {
+async function getMeals () {
   try {
     const response = await fetch(
-      "https://www.themealdb.com/api/json/v1/1/search.php?s="
+      'https://www.themealdb.com/api/json/v1/1/search.php?s='
     )
     const data = await response.json()
     allMeals = data.meals
     displayMeals(allMeals)
   } catch (error) {
-    console.error("Error fetching meals:", error)
+    console.error('Error fetching meals:', error)
   }
 }
 
-function displayMeals(meals) {
-  mealList.innerHTML = ""
+function displayMeals (meals) {
+  mealList.innerHTML = ''
 
   meals.forEach((meal) => {
-    const mealItem = document.createElement("div")
-    mealItem.classList.add("meal-item")
+    const mealItem = document.createElement('div')
+    mealItem.classList.add('meal-item')
     mealItem.innerHTML = `
             <div class="meal-image">
                 <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
@@ -32,12 +32,12 @@ function displayMeals(meals) {
                 <p>Instructions: ${meal.strInstructions}</p>
             </div>
         `
-    mealList.appendChild(mealItem);
+    mealList.appendChild(mealItem)
   })
 }
 
-searchInput.addEventListener("input", () => {
-  const searchTerm = searchInput.value.trim().toLowerCase()
+searchInput.addEventListener('input', () => {
+  const searchTerm = search-input.value.trim().toLowerCase()
   const filteredMeals = allMeals.filter((meal) =>
     meal.strMeal.toLowerCase().includes(searchTerm)
   )
