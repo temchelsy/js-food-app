156e8c0e355a66654987deef08673637c53aabe6
tem chelsy <temchelsy@gmail.com>
2024-04-30 15:24:38 +0200

    my food recipe app

--- assert/app.js
+++ assert/app.js
@@ -15,11 +15,11 @@ async function getMeals () {
   }
 }
 
-function displayMeals(meals) {
-  list.innerHTML = '';
+function displayMeals (meals) {
+  list.innerHTML = ''
   meals.forEach((meal) => {
-    const mealItem = document.createElement('div');
-    mealItem.classList.add('meal-item');
+    const mealItem = document.createElement('div')
+    mealItem.classList.add('meal-item')
     mealItem.innerHTML = `
       <h2 class='meal-name'>${meal.strMeal}</h2>
       <div class='meal-image'>
@@ -30,11 +30,10 @@ function displayMeals(meals) {
         <p>Area: ${meal.strArea}</p>
         <p>Instructions: ${meal.strInstructions}</p>
       </div>
-    `;
-    list.appendChild(mealItem);
-  });
+    `
+    list.appendChild(mealItem)
+  })
 }
-
 
 search.addEventListener('input', () => {
   const searchTerm = search.value.trim().toLowerCase()
